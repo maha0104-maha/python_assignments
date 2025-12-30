082cb29c461d96ee0afd1ae9f07e3c6085a18736
maha0104-maha <lahyavennapusa0104@gmail.com>
2025-12-30 15:39:41 +0530

    Add files via upload

--- assignment1.py
+++ assignment1.py
@@ -4,3 +4,8 @@ print("Addition:",a+b)
 print("Subtraction:",a-b)
 print("Multiplication:",a*b)
 print("Divison:",a/b)
+
+#task2
+first_name=input("enter your first name")
+last_name=input("enter your last name")
+print(f"Hello {first_name+' '+last_name}! Welcome to python program")
